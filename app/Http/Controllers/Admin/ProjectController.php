1b98e1fb9802f056fd2705bffca67bb0484dd73f
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-02-14 23:57:35 +0100

    modified method create in ProjectController

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Project;
+use App\Models\Type;
 
 class ProjectController extends Controller
 {
@@ -23,7 +24,8 @@ class ProjectController extends Controller
      */
     public function create()
     {
-        return view('admin.projects.create');
+        $types = Type::all();;
+        return view('admin.projects.create', compact('types'));
     }
 
     /**
