5f42b71820c6ff4b4b5eaef61f5da0c84d95cf6b
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-02-14 17:33:59 +0100

    created seeder for new 'types' table

--- database/migrations/2024_02_14_150109_create_types_table.php
+++ database/migrations/2024_02_14_150109_create_types_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('types', function (Blueprint $table) {
             $table->id();
             $table->string('type', 15);
-            $table->string('description');
+            $table->text('description');
             $table->string('technologies');
             $table->timestamps();
         });
